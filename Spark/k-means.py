4a7e05f02648e6b0cacab4541e51056a30219b5e
Alex <derobertoalexander@gmail.com>
2020-06-20 18:05:14 +0200

    Iterazioni implementate

--- Spark/k-means.py
+++ Spark/k-means.py
@@ -10,6 +10,10 @@ import subprocess
 #dimension = 2
 #filename = 'output.txt'
 
+#Default parameters
+maxIterations = 10
+threshold = 0.5
+
 def assignToCentroid(pointString, centroids):
 	#Convert pointString to float np.array
 	point = np.fromstring(pointString, dtype=float, sep=' ')
@@ -20,12 +24,8 @@ def assignToCentroid(pointString, centroids):
 	#Append 1 at the end to be used as counter (of points) in reduce
 	return index, np.append(point, [1])
 
-def parsePoint(row):
-	tokens = row.split(' ')
-	return np.array(tokens[:-1], dtype=float), float(tokens[-1])
-
 if __name__ == "__main__":
-	options, remainder = getopt.getopt(sys.argv[1:], 'o:n:d:', ['output=', 'num_points=', 'dimension=',])
+	options, remainder = getopt.getopt(sys.argv[1:], 'o:n:d:i:t:', ['output=', 'num_points=', 'dimension=', 'iterations=', 'threshold='])
 	print('OPTIONS :', options)
 	for opt, arg in options:
 		if opt in ('-o', '--output'):
@@ -34,6 +34,10 @@ if __name__ == "__main__":
 			points = int(arg)
 		elif opt in ('-d', '--dimension'):
 			dimension = int(arg)
+		elif opt in ('-i', '--iterations'):
+			maxIterations = int(arg)
+		elif opt in ('-t', '--threshold'):
+			threshold = float(arg)
 
 	master = "local"
 	sc = SparkContext(master, "k-Means")
@@ -46,12 +50,38 @@ if __name__ == "__main__":
 	br_centroids = sc.broadcast(centroids)
 	#Fine carico i centroidi
 	
-
-	pointStrings = sc.textFile("spark-test/points_10x2.txt")
-	centroidPointPairs = pointStrings.map(lambda x: assignToCentroid(x, br_centroids.value)).cache()#TODO: Vedere se cache qui serve
-	newCentroids = centroidPointPairs.reduceByKey(lambda x, y: np.add(x, y)).mapValues(lambda x: (np.divide(x, x[-1]))[0:-1])
+	pointStrings = sc.textFile("spark-test/points_10x2.txt_test")
 	
-	newCentroids.saveAsTextFile("spark-test/test_output")
+	iteration = 1
+	delta = float("inf")
+	
+	while True:
+		#Perform map-reduce
+		centroidPointPairs = pointStrings.map(lambda x: assignToCentroid(x, br_centroids.value)).cache()#TODO: Vedere se cache qui serve
+		newCentroidsTmp = centroidPointPairs.reduceByKey(lambda x, y: np.add(x, y)).mapValues(lambda x: (np.divide(x, x[-1]))[0:-1]).collect()
+		
+		#Compute centroids' movements
+		newCentroids = np.array([x[1] for x in newCentroidsTmp])#Build ndarray from a list of key-value pairs
+		delta = np.linalg.norm(br_centroids.value - newCentroids, axis=1).sum()
+		
+		#Broadcast new centroids
+		br_centroids = sc.broadcast(newCentroids)
+		
+		#Print obtained centroids
+		print("Obtained centroids")
+		print(br_centroids.value)
+		
+		#Check stop conditions
+		print('Delta =', delta, 'obtained at iteration', iteration)
+		if (iteration >= maxIterations) or (delta < threshold):
+			break
+		
+		iteration += 1
+	
+	#Iterative part ended
+	print('Algorithm terminated after', iteration, 'iterations. Delta value is:', delta)
+	
+	#delta.saveAsTextFile("spark-test/test_output")
 
 	start_time = time.time()
 	print("Done in %s seconds" % (time.time() - start_time))
