086decb43aa5cdd6527e13c9638278579438e1d0
Marco Bongiovanni <marco.bonjo@gmail.com>
2020-06-27 13:12:02 +0200

    Minor fix

--- openstack/main.py
+++ openstack/main.py
@@ -36,16 +36,9 @@ from flask import Flask
 from flask import request
 from flask_restful import Resource, Api
 
-IMAGE_NAME = "Cirros"
-NETWORK_NAME = "internal"
-FLAVOR_NAME = "standard"
-START_PEAK = "Jun 26 2020 12:05PM"
-END_PEAK = "Jun 26 2020 12:06PM"
-
 # Begin Flask
 app = Flask(__name__)
 api = Api(app)
-
 
 class OpenStackHandler(Resource):
     def post(self):
@@ -53,7 +46,6 @@ class OpenStackHandler(Resource):
             return "Bad request", 400
         content = request.get_json()
 
-        dateFormat = '%b %d %Y %I:%M%p'
         # compute the delay for the creation of the new server
         start_delay = (datetime.datetime.strptime(content['start'], dateFormat) - datetime.datetime.now()).total_seconds()
         print(start_delay)
@@ -62,7 +54,7 @@ class OpenStackHandler(Resource):
         print(end_delay)
 
         # check if the date are antecedent the current date
-        if start_delay < 0 or end_delay < 0 or content['start'] == content['end']:
+        if content['start'] == content['end'] or start_delay < 0 or end_delay < 0 or start_delay > end_delay:
             return "Bad request", 400
 
         server_schedule(content['image'], content['network'], content['flavor'], content['start'], content['end'], start_delay, end_delay)
@@ -75,7 +67,7 @@ api.add_resource(OpenStackHandler, '/', methods=['POST'])
 
 scheduler = None
 conn = None
-
+dateFormat = '%b %d %Y %I:%M%p'
 
 def create_server(image_name, network_name, flavor_name, start_peak, end_peak):
     global conn
@@ -97,9 +89,9 @@ def delete_server(start_peak, end_peak):
     global conn
     print("Delete Server: serv:" + start_peak + "-" + end_peak)
 
-    server = conn.compute.find_server("serv:" + start_peak + "-" + end_peak)
-
-    conn.compute.delete_server(server)
+    for server in conn.compute.servers():
+        if server.name == "serv:" + start_peak + "-" + end_peak:
+            conn.compute.delete_server(server)
 
 
 def create_flavors():
@@ -114,9 +106,9 @@ def create_flavors():
         elif f.name == "standard":
             lrg = True
     if not std:
-        std_f = conn.compute.create_flavor(name="standard", ram=128, vcpus=1, disk=1)
+        std_f = conn.compute.create_flavor(name="standard", ram=256, vcpus=1, disk=1)
     if not lrg:
-        lrg_f = conn.compute.create_flavor(name="large", ram=256, vcpus=2, disk=1)
+        lrg_f = conn.compute.create_flavor(name="large", ram=512, vcpus=2, disk=1)
 
 
 def delete_flavors():
